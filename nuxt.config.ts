9b0ec92b9c40594ac59be32a71d3ac2d65b27884
Zuby012 <amahblaise0@gmail.com>
2025-12-02 14:12:55 +0100

    berows header update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,12 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  app: {
+    head: {
+      titleTemplate: 'Berows University - %s',
+      link: [
+        {rel: 'icon', type: 'image/png', href: '/logo1.1.png'}
+      ],
+    }
+  },
 })
